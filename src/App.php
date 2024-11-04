6e53aaeeeff6efd43566c32b41a267a2032778ff
Quentin Delcourt <quentin@delcourt.be>
2024-11-05 00:03:42 +0100

    feat: display traveller's information in a widget at the top

--- src/App.php
+++ src/App.php
@@ -4,20 +4,50 @@ declare(strict_types=1);
 
 namespace Kant312\StibMivbCli;
 
+use Kant312\StibMivb\Client;
+use Kant312\StibMivb\Model\TravellersInformation;
 use PhpTui\Tui\DisplayBuilder;
-use PhpTui\Tui\Extension\Core\Shape\MapResolution;
-use PhpTui\Tui\Extension\Core\Shape\MapShape;
-use PhpTui\Tui\Extension\Core\Widget\CanvasWidget;
+use PhpTui\Tui\Extension\Core\Widget\Block\Padding;
+use PhpTui\Tui\Extension\Core\Widget\BlockWidget;
+use PhpTui\Tui\Extension\Core\Widget\GridWidget;
+use PhpTui\Tui\Extension\Core\Widget\ParagraphWidget;
+use PhpTui\Tui\Layout\Constraint;
+use PhpTui\Tui\Text\Text;
+use PhpTui\Tui\Text\Title;
+use PhpTui\Tui\Widget\Borders;
+use PhpTui\Tui\Widget\BorderType;
+use PhpTui\Tui\Widget\Direction;
 
 final class App
 {
     public static function run(): void
     {
+        $client = Client::create(getenv('API_KEY'));
+        $travellersInformationRaw = $client->latestTravellersInformation();
+        $travellersInformation = array_reduce(
+            $travellersInformationRaw,
+            fn ($carry, TravellersInformation $item) => $carry .= $item->description->french . "\n",
+            '' );
+
         $display = DisplayBuilder::default()->build();
         $display->clear();
         $display->draw(
-            CanvasWidget::fromIntBounds(-180, 180, -90, 90)
-            ->draw(MapShape::default()->resolution(MapResolution::High))
+            GridWidget::default()
+                ->direction(Direction::Vertical)
+                ->constraints(
+                    Constraint::percentage(20),
+                    Constraint::percentage(80),
+                )
+                ->widgets(
+                    BlockWidget::default()
+                        ->titles(Title::fromString('Travellers information'))
+                        ->padding(Padding::all(2))
+                        ->borders(Borders::ALL)
+                        ->borderType(BorderType::Rounded)
+                        ->widget(ParagraphWidget::fromText(Text::parse(
+                            $travellersInformation
+                        )))
+                )
         );
     }
 }
